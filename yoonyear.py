ae263272a5a0e9953e98c354bd2b16311eefe207
FinDucks <tjtmdejr1216@naver.com>
2022-08-18 13:25:40 +0900

    feat : create yooonyear
    condition:Detect leap year

---
+++ yoonyear.py
@@ -0,0 +1,11 @@
+year=insert("년도입력 : ")
+if year % 400 = 0:
+    print("윤녀입니다.")
+elif year % 100 =0:
+    print("유년이 아닙니다.")
+elif year %4 = 0:
+    print("윤년입니다.")
+else:
+    print("유년이 아닙니다.")
+
+
